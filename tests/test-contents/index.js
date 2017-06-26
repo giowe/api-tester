149bfe10a53c638272a078ed7f2f10efe3b79b9c
nico-ubuntu <nico.fuccella@gmail.com>
2017-06-26 11:48:06 +0200

    ggg

--- tests/test-contents/index.js
+++ tests/test-contents/index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const promise = new Promise ((resolve, reject) => {
   const config = JSON.parse(fs.readFileSync('./config.json'));
   const sample = JSON.parse(fs.readFileSync('./sample.json'));
-
+  
   // eventuale auth
   resolve(config, sample);
 });

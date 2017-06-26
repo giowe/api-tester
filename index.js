57ea7a69927b3ce4f4bb3a10e625013197d3585b
nico-ubuntu <nico.fuccella@gmail.com>
2017-06-26 11:10:02 +0200

    ggg

---
+++ index.js
@@ -0,0 +1,11 @@
+const expect = require('expect');
+
+const testContents = require('./tests/tests-contents');
+const samplesTestContents = JSON.parse(require('./tests/test-contents/sample.json'));
+
+testContents.then((res) => {
+  // vari expects
+  // res deve essere uguale a samplesTestContents 
+}).catch(
+  // thow error request failed
+);
---
+++ tests/test-contents/index.js
@@ -0,0 +1,5 @@
+const promise = new Promise = (
+
+);
+
+module.exports = promise;

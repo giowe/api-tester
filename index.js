4f2d40e8ee52dc2ebdb4eee4a87296fb7d391b28
nico-ubuntu <nico.fuccella@gmail.com>
2017-06-26 16:29:30 +0200

    ggg

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
 "use strict";
 
 const request = require("request");
-const { expect, it } = require('chai');
-const testUsers = require('./tests/test-users');
+const { expect } = require('chai');
+const chooseTest = require('./utils/choose-test')
 
-testUsers.then((result) => {
-  const sample = result.sample
-  const {method, host, path} = result.config
+chooseTest.then(result => {
+  const testName = result['Test'];
+  const test = require(`${process.cwd()}/${testName}`);
+  test.then((result) => {
+    const sample = result.sample
+    const {method, host, path} = result.config
 
-  const opt = {
-    method,
-    uri : `${host}${path}`
-  }
-  // vari expects
-  request(
-    opt, (err, res, body) => {
-      if (err) {
-        console.log(err);
-      }
-      else {
-        body = JSON.parse(body);
-        expect(body).to.deep.equal(sample);
-        console.log("Fatto");
-      }
-    } 
-  )
+    const opt = {
+      method,
+      uri : `${host}${path}`
+    }
+    // vari expects
+
+    request(
+      opt, (err, res, body) => {
+        if (err) {
+          console.log(err);
+        }
+        else {
+          body = JSON.parse(body);
+          expect(body).to.deep.equal(sample);
+        }
+      } 
+    )
 // res deve essere uguale a samplesTestContents
-}).catch((err) => console.log('sono qui', err)
-  // thow error request failed
-);
+  }).catch((err) => console.log('sono qui', err)
+    // thow error request failed
+  );
+}).catch((err) => {
+  console.log(err)
+});

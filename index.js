39e535fd2eeda5809f6441c72ccb53bbf6bc69d0
nico-ubuntu <nico.fuccella@gmail.com>
2017-06-26 17:45:23 +0200

    ggg

--- index.js
+++ index.js
@@ -1,40 +1,44 @@
-"use strict";
+'use strict';
 
-const request = require("request");
+const request = require('request');
 const { expect } = require('chai');
-const chooseTest = require('./utils/choose-test')
-const init = require('./init.js')
+const chooseTest = require('./utils/choose-test');
+const init = require('./init.js');
+const chalk = require('chalk');
 
 init();
+
+const verbose = true;
 
 chooseTest.then(result => {
   const testName = result['Test'];
   const test = require(`${process.cwd()}/${testName}`);
   test.then((result) => {
-    const sample = result.sample
-    const {method, host, path} = result.config
+    const sample = result.sample;
+    const {method, host, path} = result.config;
 
     const opt = {
       method,
       uri : `${host}${path}`
-    }
-    // vari expects
+    };
 
     request(
       opt, (err, res, body) => {
         if (err) {
-          console.log(err);
+          console.log(chalk.red(err));
         }
         else {
           body = JSON.parse(body);
-          expect(body).to.deep.equal(sample);
+          if (body !== sample) {
+            const errorMessage = `Received \n${chalk.red(JSON.stringify(body))}, but should be equal to \n${chalk.green(JSON.stringify(sample))}`;
+            console.log(errorMessage);
+            process.exit();
+          } 
+          console.log(chalk.green('Done!!!'));
         }
       } 
-    )
-// res deve essere uguale a samplesTestContents
-  }).catch((err) => console.log('sono qui', err)
-    // thow error request failed
-  );
+    );
+  }).catch((err) => console.log(chalk.red('There is an error in your test configuration.')));
 }).catch((err) => {
-  console.log(err)
+  console.log(err);
 });

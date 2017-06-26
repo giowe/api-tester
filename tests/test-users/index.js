26d0af7497ce051209e954fd8adb89c589482bb3
nico-ubuntu <nico.fuccella@gmail.com>
2017-06-26 12:13:27 +0200

    ggg

--- tests/test-users/index.js
+++ tests/test-users/index.js
@@ -4,7 +4,9 @@ const fs = require('fs');
 const myPromise = new promise((resolve, reject) => {
 	const config = JSON.parse(fs.readFileSync('./config.json'));
 	const sample = JSON.parse(fs.readFileSync('./sample.json'));
-	
+	const urlSecret = JSON.parse(fs.readFileSync('./url-secret.json'));
+  // eventuale auth
+	Object.assign(config, urlSecret);
 	resolve(config, sample);
 	});
 

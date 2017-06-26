cb37b0c3e9e282bb5e8929fe30fe0991671d9be9
Ng2k <nicola.ng2k@gmail.com>
2017-06-26 11:57:22 +0200

    Mods

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const expect = require('expect');
-
+const fs = require ('fs');
 const testContents = require('./tests/test-contents');
-const samplesTestContents = JSON.parse(require('./tests/test-contents/sample.json'));
+const samplesTestContents = JSON.parse(fs.Sync('./tests/test-contents/sample.json'));
+const opt = JSON.parse(require('./confing.json'));
+const http = require('http');
 
-testContents.then((res) => {
+
+testContents.then((res, sample) => {
   // vari expects
-  // res deve essere uguale a samplesTestContents
+  expect(res).toBe(sample);
+// res deve essere uguale a samplesTestContents
 }).catch(
   // thow error request failed
 );

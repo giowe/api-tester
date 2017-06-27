1699eb612f5abffe99d76ddd42ccafbe65b6f700
Ng2k <nicola.ng2k@gmail.com>
2017-06-27 16:16:11 +0200

    ggg

--- tests/api-tester-create-rel-embedsMany.js
+++ tests/api-tester-create-rel-embedsMany.js
@@ -1,69 +1,75 @@
-"use strict";
+'use strict';
 
 const clean = reuqire("../schema/clean");
 
 const auth = require('./auth.js');
-
-const baseTest = require('./baseTestNoClean');
-
-const payload = {
-  permalink: 'embedsMany-test',
-  lang: 'it',
-  asides: [
-    {
-      handler: 'label1',
-      lang: 'it',
-      label: 'label 1',
-      body: 'body 1'
-    },
-    {
-      handler: 'label2',
-      lang: 'it',
-      label: 'label 2',
-      body: 'body 2'
-    },
-    {
-      handler: 'label3',
-      lang: 'it',
-      label: 'label 3',
-      body: 'body 3'
-    },
-    {
-      handler: 'label4',
-      lang: 'it',
-      label: 'label 4',
-      body: 'body 4'
-    },
-    {
-      handler: 'label5',
-      lang: 'it',
-      label: 'label 5',
-      body: 'body 5'
-    }
-  ],
-  type: {
-    permalink: 'pagine',
-    lang: 'it'
-  }
-};
-
 const path = 'contents/contents';
 const method = 'POST';
 
-const config = {
-  "method": method,
-  "path": path,
-  "headers": {
-    "Content-Type": "application/json",
-    "Authorization": auth
+const params = {
+  method,
+  path,
+  input:
+  {
+    headers: {
+      'content-type': "application/json",
+    },
+    body: {
+      permalink: 'embedsMany-test',
+      lang: 'it',
+      asides: [
+        {
+          handler: 'label1',
+          lang: 'it',
+          label: 'label 1',
+          body: 'body 1'
+        },
+        {
+          handler: 'label2',
+          lang: 'it',
+          label: 'label 2',
+          body: 'body 2'
+        },
+        {
+          handler: 'label3',
+          lang: 'it',
+          label: 'label 3',
+          body: 'body 3'
+        },
+        {
+          handler: 'label4',
+          lang: 'it',
+          label: 'label 4',
+          body: 'body 4'
+        },
+        {
+          handler: 'label5',
+          lang: 'it',
+          label: 'label 5',
+          body: 'body 5'
+        }
+      ],
+      type: {
+        permalink: 'pagine',
+        lang: 'it'
+      }
+    }
   }
 };
 const sample = require('./api-tester-create-rel-embedsMany-sample.json');
 
-clean.then(res => {
-  const promise = new Promise((resolve, reject) => {
-    resolve(config, sample);
-  });
-  module.exports = promise;
+module.exports = () => new Promise((resolve, reject) => {
+  clean(true)
+    .then((data) => {
+      console.log(data);
+      return auth();
+    })
+    .then((token) => {
+      params.input.headers.authorization = token;
+      resolve(params);
+    })
+    .catch((err) => {
+      reject(err);
+    });
 });
 

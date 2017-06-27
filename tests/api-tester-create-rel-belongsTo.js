c19d98f65cf0a991b5b209c94aa70b1962f0e7f8
Ng2k <nicola.ng2k@gmail.com>
2017-06-27 16:04:08 +0200

    Mods

--- tests/api-tester-create-rel-belongsTo.js
+++ tests/api-tester-create-rel-belongsTo.js
@@ -4,50 +4,40 @@ const clean = reuqire("../schema/clean");
 
 const auth = require('./auth.js');
 
-const baseTest = require('./baseTestNoClean');
-
-const payload = {
-  permalink: 'belongsTo-test',
-  lang: 'it',
-  type: {
-    permalink: 'pagine',
-    lang: 'it'
-  }
-};
-
 const path = 'contents/contents';
 const method = 'POST';
+const output = require('./api-tester-create-rel-belongsTo-sample.json');
 
-const config = {
-  "method": method,
-  "path": path,
-  "headers": {
-    "Content-Type": "application/json",
-    "Authorization": auth
-  }
+const params = {
+  method,
+  path,
+  input: {
+    headers: {
+      'content-type': 'application/js'
+    },
+    body: {
+      permalink: 'belongsTo-test',
+      lang: 'it',
+      type: {
+        permalink: 'pagine',
+        lang: 'it'
+      }
+    },
+  },
+  output
 };
-const sample = require('./api-tester-create-rel-belongsTo-sample.json');
 
-clean.then(res => {
-  const promise = new Promise((resolve, reject) => {
-    resolve(config, sample);
-  });
-  module.exports = promise;
+module.exports = () => new Promise((resolve, reject) => {
+  clean(true)
+    .then((data) => {
+      console.log(data);
+      return auth();
+    })
+    .then((token) => {
+      params.input.headers.authorization = token;
+      resolve(params);
+    })
+    .catch((err) => {
+      reject(err);
+    });
 });
-
-const test = (terminatePool = false) =>
-  baseTest(
-    path,
-    method,
-    JSON.stringify(payload),
-    terminatePool
-  );
-
-// eslint-disable-next-line
-if (process.argv[1] === __filename) {
-  test(true)
-    .then(data => { console.log(data); })
-    .catch(error => { console.error(error); });
-}
-
-module.exports = test;

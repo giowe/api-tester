cb0cc74eb2950f32854d5238ff518447ab5a1c3a
nico-ubuntu <nico.fuccella@gmail.com>
2017-06-28 17:55:44 +0200

    ggg

---
+++ getTestStatus.js
@@ -0,0 +1,9 @@
+const getErrorStatus = (result, sample) => {
+
+}
+
+// deve returnare:
+// 0 => tutte le chiavi sono giuste
+// 1 => almeno una chiave e giusta
+// 2 => nessuna chiave e giusta
+module.exports = getErrorStatus;

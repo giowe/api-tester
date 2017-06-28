7841e71230f04925335af37719427a6154636aaf
Ng2k <nicola.ng2k@gmail.com>
2017-06-28 12:41:18 +0200

    ggg

--- getErrorMessage.js
+++ getErrorMessage.js
@@ -1,12 +1,55 @@
+'use strict';
+
 const chalk = require('chalk');
-var resultG;
+
+const isEmptyObject = function(obj) {
+  for (let name in obj) return false;
+  return true;
+};
+
+// quello sbagliato => obj2
+const diff = function(obj1, obj2) {
+  const result = {};
+  let change;
+  for (let key in obj1) {
+    if (typeof obj2[key] == 'object' && typeof obj1[key] == 'object') {
+      change = diff(obj1[key], obj2[key]);
+      if (isEmptyObject(change) === false) {
+        result[key] = change;
+      }
+    }
+    else if (obj2[key] != obj1[key]) {
+      result[key] = obj2[key];
+    }
+  }
+  return result;
+};
 const getErrorMessage = (result, sample, type) => {
   let errorMessage = '';
+  console.log(diff(result, sample));
   if (type === 'application/json') {
-    result.
+    const errorData =diff(sample, result);
+    console.log(chalk.red(errorData));
   }
 };
 
-console.log(typeof(resultG))
+const obj1 = {
+  name: 'pippo',
+  lastname: 'ciao',
+  d:{
+    l: 's'
+  }
+};
+
+const obj2 = {
+  name: 'pluto',
+  lastname: 'ciao',
+  d: {
+    l: 'a'
+  }
+};
+const errorData = diff(obj1, obj2);
+
+console.log(chalk.red(JSON.stringify(errorData)));
 
 module.exports = getErrorMessage;

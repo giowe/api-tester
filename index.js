77d2c8656c8d54affbc6d39bb4a1a6b95540d71f
nico-ubuntu <nico.fuccella@gmail.com>
2017-06-28 12:21:30 +0200

    ggg

--- index.js
+++ index.js
@@ -2,114 +2,60 @@
 
 const request = require('request');
 const { expect } = require('chai');
-// const chooseTest = require('./utils/choose-test');
 const init = require('./init.js');
 const chalk = require('chalk');
-const getErrorMessage = require('./getErrorMessage');
+// const getErrorMessage = require('./getErrorMessage');
 const columnify = require('columnify');
+const Q = require('q');
+// init().then((params) => { console.log(params); });
+const promiseWaterfall = require('promise.waterfall');
 
-const verbose = true;
+init().then((params) => {
+  console.log(params)
+  const { verbose, tests } = params;
+  const wrappedTests = tests.map((test) => () => new Promise((resolve, reject) => {
+    test()
+      .then((params) => {
+      console.log('ciao')
+      const {method, uri, output, input} = params;
+      const {headers, body} = input;
+      const opt = {
+        method,
+        uri,
+        headers,
+        json: body
+      };
+      request(
+        opt, (err, res, body) => {
+          if (err) {
+            console.log(chalk.red(err));
+            reject(err);
+          }
+          else {
+            try {
+              const outputBody = output.body;
+              expect(body).to.deep.equal(outputBody);
+              console.log(chalk.green('Done!!!'));
+              resolve();
+              return next();
+            } catch (err) {
+              const type = res.headers['content-type'].split('; ')[0];
+              console.log('result: ', body);
+              console.log('sample: ', output);
+              reject(err);
+              // getErrorMessage(body, sample, type);
+              // console.log(errorMessage);
+            }
+          }
+        }
+      );
+    })
+      .catch((err) => console.log(err));
+  }));
+  const waterfall = () => promiseWaterfall(wrappedTests);
 
-const tests = [require('./tests/api-tester-create-no-rel'), require('./tests/api-tester-create-rel-belongsTo')];
+  waterfall()
+    .then(() => console.log('fatto'))
+    .catch((err) => console.log(err));
 
-// init().then((tests) => {
-
-// const f = tests.map(test => 
-//   test().then((params) => {
-//     const { method, uri, output, input } = result.config;
-//     const { headers, body } = input 
-//     const opt = {
-//       method,
-//       uri
-//     };
-//     request(
-//       opt, (err, res, body) => {
-//         if (err) {
-//           console.log(chalk.red(err));
-//         }
-//         else {
-//           body = JSON.parse(body);
-//           try {
-//             expect(body).to.deep.equal(sample)
-//             console.log(chalk.green('Done!!!'));
-//           } catch (err) {
-//             const type = res.headers['content-type'].split('; ')[0];
-//             // getErrorMessage(body, sample, type);
-//           // console.log(errorMessage);
-//           }
-//         }
-//       }
-//     );
-//   })
-//     .catch(error => { console.error(error); })
-// );
-
-const p1 = () => (new Promise((resolve, reject) => {
-  const msg = 'benvenuto in p1';
-  resolve(msg)
-}))
-
-
-const p2 = () => (new Promise((resolve, reject) => {
-  const msg = 'benvenuto in p2';
-  resolve(msg)
-}))
-
-const p3 = () => (new Promise((resolve, reject) => {
-  const msg = 'benvenuto in p3';
-  resolve(msg)
-}))
-
-const f = [p1, p2, p3];
-
-const newF = f.map(promise => new Promise((resolve, reject) => {
-  promise().then((msg) => {
-    console.log(msg)
-    resolve();
-  });
-}))
-
-// f.reduce((cur, next) => 
-//   cur
-//     .then()
-//     .then(next),
-//   Promise.resolve()
-// );
-
-newF.reduce((p, fn) => p.then(fn), Promise.resolve());
-
-
-// });
-// chooseTest.then(result => {
-//   const testName = result['Test'];
-//   const test = require(`${process.cwd()}/${testName}`);
-//   test.then((result) => {
-//     const sample = result.sample;
-//     const { method, host, path } = result.config;
-//     const opt = {
-//       method,
-//       uri : `${host}${path}`
-//     };
-
-//     request(
-//       opt, (err, res, body) => {
-//         if (err) {
-//           console.log(chalk.red(err));
-//         }
-//         else {
-//           body = JSON.parse(body);
-//           try {
-//             expect(body).to.deep.equal(sample)
-//             console.log(chalk.green('Done!!!'));
-//         } catch (err) {
-//           const type = res.headers['content-type'].split('; ')[0];
-//           getErrorMessage(body, sample, type);
-//             // console.log(errorMessage);
-//           }
-//         }
-//       } 
-//     );
-//   }).catch((err) => console.log(chalk.red('There is an error in your test configuration.')));
-// }).catch((err) => {
-//   console.log(err);
-// });
+}).catch((err) => console.log(err));
--- tests/fake-core.js
+++ tests/fake-core.js
@@ -2,7 +2,7 @@
 
 const test = require('./api-tester-create-no-rel');
 
-test.then((data) => {
+test().then((data) => {
   console.log('bene', data);
 }).catch((err) => {
   console.log('male', err);

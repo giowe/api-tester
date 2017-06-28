3a5b48fb408c32690a5bb2767486824cf47e421e
Ng2k <nicola.ng2k@gmail.com>
2017-06-28 10:49:25 +0200

    ggg

--- tests/test-users-delete/api-tester-user-delete.js
+++ tests/test-users-delete/api-tester-user-delete.js
@@ -1,19 +1,36 @@
 'use strict';
 
+const clean = require('../schema/clean');
+
 const auth = require('../auth.js');
-const config = {
-  method : 'DELETE',
-  path : '/principals/users',
-  headers: {
-    'Content-Type': 'application/json',
-    Authorization: auth
-  }
+const output = require('./api-tester-user-delete-sample.json');
+const method ='DELETE';
+const path ='/principals/users/types';
+const urlSecret = 'https://api-staging-f3.soluzionifutura.it';
+const urlJoin = require('url-join');
+
+const params = {
+  method,
+  uri: urlJoin(urlSecret, path),
+  input: {
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  },
+  output,
 };
-const sample = require('./sample.json');
 
-const promise = new Promise((resolve, reject) => {
-  resolve(config, sample);
+module.exports = () => new Promise((resolve, reject) => {
+  clean(true)
+    .then((data) => {
+      console.log(data);
+      return auth();
+    })
+    .then((token) => {
+      params.input.headers.authorization = token;
+      resolve(params);
+    })
+    .catch((err) => {
+      reject(err);
+    });
 });
-
-module.exports = promise;
-

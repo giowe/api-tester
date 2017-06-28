b8a72d369ee89c9f0ff320813e843d8994800a9d
Ng2k <nicola.ng2k@gmail.com>
2017-06-28 18:35:06 +0200

    ggg

--- getTestStatus.js
+++ getTestStatus.js
@@ -1,6 +1,38 @@
-const getErrorStatus = (result, sample) => {
+'use strict';
 
-}
+const { expect } = require('chai');
+var status;
+var statusExit;
+
+const getErrorStatus = (result, sample) => {
+  try{
+    expect(result.status).to.deep.equal(sample.status);
+  }catch(err) {
+    status++;
+  }
+  try{
+    expect(result.headers).to.deep.equal(sample.headers);
+  }catch(err) {
+    status++;
+  }
+  try{
+    expect(result.body).to.deep.equal(sample.body);
+  }catch(err) {
+    status++;
+  }
+  try{
+    expect(result.headersKeys).to.deep.equal(sample.headersKeys);
+  }catch(err) {
+    status++;
+  }
+  try{
+    expect(result.bodyKeys).to.deep.equal(sample.bodyKeys);
+  }catch(err) {
+    status++;
+  }
+  if (status >= 1 || status <= 4) statusExit = 1;
+  else if 
+};
 
 // deve returnare:
 // 0 => tutte le chiavi sono giuste

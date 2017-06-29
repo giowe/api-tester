acca9c4a0ad5c250471b239673d8b8f0c41b2a8a
nico-ubuntu <nico.fuccella@gmail.com>
2017-06-29 09:51:19 +0200

    ggg

--- schema/conf.js
+++
@@ -1,7 +0,0 @@
-"use strict"
-
-module.exports = {
-  host: "rds-staging.soluzionifutura.it",
-  user: "staginguser",
-  password: "Kddsb21xc3q{NM+2g"
-}
--- tests/fake-core.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const test = require('./api-tester-create-no-rel');
-
-test().then((data) => {
-  console.log('bene', data);
-}).catch((err) => {
-  console.log('male', err);
-});

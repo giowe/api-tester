9c6f679939ab5fbfd20ec5d731b27f83dbfe3fd8
nico-ubuntu <nico.fuccella@gmail.com>
2017-06-29 11:02:26 +0200

    ggg

--- init.js
+++ init.js
@@ -4,6 +4,39 @@ const fs = require("fs");
 const localDir = process.cwd();
 const path = require('path');
 
+const testsValidation = (
+  testsNames
+) => {
+  const testsValidated = [];
+  testsNames.forEach((
+    testName
+  ) => {
+    if (testName.slice(0, 2) !== 't-') {
+      console.log(`Test "${testName}" not valid`);
+      process.exit();
+    }
+    testsValidated.push(testName);
+  });
+  return testsValidated;
+};
+
+const getTestsObj = (
+  tests
+) => {
+  const testsObj = {};
+  tests.forEach(testName => {
+    const test = require(path.join(localDir, testName));
+    if (typeof test === 'object' && Array.isArray(test.tests)) {
+      Object.assign(testsObj, getTestsObj(test.tests));
+    } else if (typeof test === 'function') {
+      testsObj[testName] = test;
+    } else {
+      console.log(`Test "${testName}" not valid`);
+      process.exit();
+    }
+  });
+};
+
 if (argv.v || argv.version) {
   const pkg = require('./package.json');
   console.log(pkg.name, pkg.version);
@@ -15,12 +48,12 @@ const params = {
   tests: {}
 };
 
-argv._.forEach((testPath) =>{
-    params.tests[testPath] = require(path.join(localDir,testPath));
-});
+
+const testsValidated = testsValidation(argv._);
+Object.assign(params.tests, getTestsObj(testsValidated));
 
 const choices = fs.readdirSync(localDir).filter(file => {
-  return file.slice(0, 11) === 't-' && file.slice(-3) === '.js';
+  return file.slice(0, 2) === 't-' && file.slice(-3) === '.js';
 });
 
 const question = {
@@ -29,25 +62,20 @@ const question = {
   message: 'Select the test to execute: ',
   choices,
   default: choices[0]
-}
+};
 
 if (choices.length === 0) {
-  console.log('There aren\'t any tests in this folder');
-  process.exit()
+  console.log('There aren\'t any valid tests in this folder');
+  process.exit();
 }
 
 module.exports = () => new Promise((resolve, reject) => {
   if(Object.keys(params.tests).length === 0){
     inquirer.prompt(question)
       .then((result) => {
-        Object.assign(
-          params.tests,
-          {
-            [result.Test]: require(path.join(localDir, result.Test))
-          }
-        );
+        Object.assign(params.tests, getTestsObj([result.Test]));
         resolve(params);
-    });
+      });
   } else {
     resolve(params);
 }});
--- tester.js
+++ tester.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const request = require('request');
-const { expect } = require('chai');
 const init = require('./init.js');
 const chalk = require('chalk');
 const getErrorMessage = require('./getErrorMessage');
@@ -16,110 +15,112 @@ const testsFailed = [];
 init().then((params) => {
   const { verbose, tests } = params;
   const testsNames = Object.keys(tests);
-  const wrappedTests = Object.values(tests).map((test, index) => () => new Promise((resolve, reject) => {
-    test()
-      .then((params) => {
-      const { method, uri, output, input } = params;
-      const { headers, body } = input;
-      const opt = {
-        method,
-        uri,
-        headers,
-        json: body
-      };
-      request(
-        opt, (err, res, body) => {
-          if (err) {
-            if (verbose) {
-              console.log(chalk.red(err));
-              console.log(chalk.cyan(`Processing "${testsNames[index]}"...`));
-            }
-            else {
-              process.stdout.write(chalk.cyan(`Processing "${testsNames[index]}"... `));
-              process.stdout.write(chalk.red('\u2718\n'));
-            }
-            console.log(' ------------------------------------------------------------------ ');
-            testsFailed.push(testsNames[index]);
-            resolve();
-          }
-          else {
-            try {
-              const outputBody = output.body;
-              if (typeof body !== 'object') body = JSON.parse(body);
-              const result = {
-                status: res.statusCode,
-                headers: res.headers,
-                body,
-                headersKeys: Object.keys(res.headers),
-                bodyKeys: Object.keys(body)
-	      };
-	      console.log('result', result);
-	      console.log('output', output);
-              const errorStatus = getTestStatus(result, output);
-              const testName = testsNames[index];
-              switch (errorStatus) {
-                case 0: {
-                  testsPassed.push(test);
-                  break;
+  const wrappedTests = Object.values(tests).map((test, index) => () => {
+    return new Promise((resolve, reject) => {
+      test()
+        .then((params) => {
+          const {method, uri, output, input} = params;
+          const {headers, body} = input;
+          const opt = {
+            method,
+            uri,
+            headers,
+            json: body
+          };
+          request(
+            opt, (err, res, body) => {
+              if (err) {
+                if (verbose) {
+                  console.log(chalk.red(err));
+                  console.log(chalk.cyan(`Processing "${testsNames[index]}"...`));
                 }
-                case 1: {
-                  testsWarned.push(test);
-                  break;
+                else {
+                  process.stdout.write(chalk.cyan(`Processing "${testsNames[index]}"... `));
+                  process.stdout.write(chalk.red('\u2718\n'));
                 }
-                case 2: {
-                  testsFailed.push(test);
-                  break;
-                }
-                default: {
-                  console.log("There is a problem in errorStatus function, ", errorStatus);
-                }
-              }
-              if (verbose) {
-                console.log(pretty(outputBody));
-                console.log(chalk.cyan(`Processing "${testsNames[index]}"...`));
+                console.log(' ------------------------------------------------------------------ ');
+                testsFailed.push(testsNames[index]);
+                resolve();
               }
               else {
-                process.stdout.write(chalk.cyan(`Processing "${testsNames[index]}"... `));
-                switch (errorStatus) {
-                  case 0: {
-                    process.stdout.write(chalk.green('\u2714\n'));
-                    break;
+                try {
+                  const outputBody = output.body;
+                  if (typeof body !== 'object') body = JSON.parse(body);
+                  const result = {
+                    status: res.statusCode,
+                    headers: res.headers,
+                    body,
+                    headersKeys: Object.keys(res.headers),
+                    bodyKeys: Object.keys(body)
+                  };
+                  console.log('result', result);
+                  console.log('output', output);
+                  const errorStatus = getTestStatus(result, output);
+                  const testName = testsNames[index];
+                  switch (errorStatus) {
+                    case 0: {
+                      testsPassed.push(test);
+                      break;
+                    }
+                    case 1: {
+                      testsWarned.push(test);
+                      break;
+                    }
+                    case 2: {
+                      testsFailed.push(test);
+                      break;
+                    }
+                    default: {
+                      console.log("There is a problem in errorStatus function, ", errorStatus);
+                    }
                   }
-                  case 1: {
-                    process.stdout.write(chalk.yellow('\u2717\n'));
-                    break;
+                  if (verbose) {
+                    console.log(pretty(outputBody));
+                    console.log(chalk.cyan(`Processing "${testsNames[index]}"...`));
                   }
-                  case 2: {
+                  else {
+                    process.stdout.write(chalk.cyan(`Processing "${testsNames[index]}"... `));
+                    switch (errorStatus) {
+                      case 0: {
+                        process.stdout.write(chalk.green('\u2714\n'));
+                        break;
+                      }
+                      case 1: {
+                        process.stdout.write(chalk.yellow('\u2717\n'));
+                        break;
+                      }
+                      case 2: {
+                        process.stdout.write(chalk.red('\u2718\n'));
+                        break;
+                      }
+                      default: {
+                        console.log("There is a problem in errorStatus function, ", errorStatus);
+                      }
+                    }
+                  }
+                  console.log(' ------------------------------------------------------------------ ');
+                  resolve();
+                } catch (err) {
+                  const type = res.headers['content-type'].split('; ')[0];
+                  testsFailed.push(testsNames[index]);
+                  if (verbose) {
+                    getErrorMessage(body, output.body, type);
+                    console.log(chalk.cyan(`Processing "${testsNames[index]}"...`));
+                  }
+                  else {
+                    process.stdout.write(chalk.cyan(`Processing "${testsNames[index]}"... `));
                     process.stdout.write(chalk.red('\u2718\n'));
-                    break;
                   }
-                  default: {
-                    console.log("There is a problem in errorStatus function, ", errorStatus);
-                  }
+                  console.log(' ------------------------------------------------------------------ ');
+                  resolve();
                 }
               }
-              console.log(' ------------------------------------------------------------------ ');
-              resolve();
-            } catch (err) {
-              const type = res.headers['content-type'].split('; ')[0];
-              testsFailed.push(testsNames[index]);
-              if (verbose) {
-                getErrorMessage(body, output.body, type);
-                console.log(chalk.cyan(`Processing "${testsNames[index]}"...`));
-              }
-              else {
-                process.stdout.write(chalk.cyan(`Processing "${testsNames[index]}"... `));
-                process.stdout.write(chalk.red('\u2718\n'));
-              }
-              console.log(' ------------------------------------------------------------------ ');
-              resolve();
             }
-          }
-        }
-      );
+          );
+        })
+        .catch((err) => console.log(err));
     })
-      .catch((err) => console.log(err));
-  }));
+  });
   const waterfall = () => promiseWaterfall(wrappedTests);
 
   waterfall()

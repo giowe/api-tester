c9f5efd840bee012c07ffff7be098612d92dd123
Ng2k <nicola.ng2k@gmail.com>
2017-06-29 21:32:20 +0200

    ggg

--- getTestStatus.js
+++ getTestStatus.js
@@ -1,94 +1,60 @@
 'use strict';
 
-const deepEql = require("deep-eql");
 let statusError = 0;
 let statusExit = -1;
 
 const tryStatus = (res, sample) => {
-  if(!deepEql(res, sample)) statusError++;
-};
-
-const tryStatusObj = (res, sample) => {
-  if(!deepEql(res, sample)) statusError++;
-};
-
-const tryStatusKeys = (resKeys, sampleKeys) => {
-  if(!deepEql(resKeys, sampleKeys)) statusError++;
+  if (JSON.stringify(res) === JSON.stringify(sample)){
+    statusError++;
+  }
 };
 
 const getStatusError = (result, sample) => {
-
   const sampleKeysLength = Object.keys(sample).length;
+
   if (sample.status) {
-    tryStatus(result.status, sample.status);
-  } else if(sample.status && !result.status) {
-    statusError++;
+    if (!result.status) statusError++;
+    else tryStatus(result.status, sample.status);
   }
 
   if (sample.headers) {
-    tryStatusObj(result.headers, sample.headers,);
-  }else if(sample.headers && !result.headers) {
-    statusError++;
+    if (!result.headers) statusError++;
+    else tryStatus(result.headers, sample.headers,);
   }
 
   if (sample.body) {
-    tryStatusObj(result.body, sample.body);
-  } else if(sample.body && !result.body) {
-    statusError++;
+    if (!result.body) statusError++;
+    else tryStatus(result.body, sample.body);
   }
 
   if (sample.headersKeys) {
-    if (result.headersKeys.length === sample.headersKeys.length) {
-      tryStatusKeys(result.headersKeys, sample.headersKeys);
-    } else {
-      statusError++;
+    if (!result.headersKeys) statusError++;
+    else {
+      if (result.headersKeys.length === sample.headersKeys.length) {
+        tryStatus(result.headersKeys, sample.headersKeys);
+      } else {
+        statusError++;
+      }
     }
-  } else if(sample.headersKeys && !result.headersKeys) {
-    statusError++;
   }
 
   if (sample.bodyKeys) {
-    if (result.bodyKeys.length === sample.bodyKeys.length) {
-      tryStatusKeys(result.bodyKeys, sample.bodyKeys);
-  } else {
-      statusError++;
+    if (!result.bodyKeys) statusError++;
+    else {
+      if (result.bodyKeys.length === sample.bodyKeys.length) {
+        tryStatus(result.bodyKeys, sample.bodyKeys);
+      } else {
+        statusError++;
+      }
     }
-  } else if(sample.bodyKeys && !result.bodyKeys){
-    statusError++;
   }
 
   if (statusError > 0 && statusError < sampleKeysLength) statusExit = 1;
-  else if (statusError === 0) statusExit = 0;
-  else if (statusError === sampleKeysLength) statusExit = 2;
+  else if (statusError === 0) statusExit = 2;
+  else if (statusError === sampleKeysLength) statusExit = 0;
 
   return statusExit;
 };
-
-/*const resultPROVA  = {
-  headers:{
-
-  },
-  body: {
-
-  },
-};
-
-const samplePROVA = {
-  headers:{
-
-  },
-  body: {
-
-  },
-  headersKey : [
-
-  ]
-};
-
-const temp = getErrorstatusError(resultPROVA, samplePROVA);
-
-console.log(temp);
-*/
 
 // deve returnare:
 // 0 => tutte le chiavi sono giuste

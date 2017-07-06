05c0d9d861deb30648f92f97f9c01e6fc31adbce
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2017-07-06 11:25:01 +0200

    test

--- main.js
+++ main.js
@@ -111,12 +111,12 @@ module.exports = (tests, options) => new Promise((resolve, reject) => {
           resolve();
         })
         .catch(({ name, err }) => {
-          if (name) console.log(chalk.red(`*** CONFIGURATION ERRORS IN TEST ${name} ***`));
+          if (name && err) console.log(chalk.red(`*** CONFIGURATION ERRORS IN TEST ${name} ***`));
           reject(err);
         });
     })
     .catch(({ name, err }) => {
-      if (name) console.log(chalk.red(`*** ERRORS IN TEST ${name} ***`));
+      if (name && err) console.log(chalk.red(`*** ERRORS IN TEST ${name} ***`));
       reject(err);
     });
 }).catch((err) => { console.log(err); });
